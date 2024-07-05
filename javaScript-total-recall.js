d418102f87130cab14cb691fabc4047625fe2bf9
y0kis <yokish1986@gmail.com>
2024-07-05 12:24:39 -0400

    STRINGS

--- javaScript-total-recall.js
+++ javaScript-total-recall.js
@@ -4,14 +4,18 @@
 //2. How do we change the value of a variable?
 // the first and easiest way of changing a value of a variable is simply to reaasign it:
 //example, let numberOfCats = 3; numberOfCats = 4;
-//3. what are declare, assign, define:
-// declare use var(only in older programs), let, const
-// assign using the assignment opperator
-// define a variable is to give the variable a value.
-//5. what is pseudocoding and why should you do it?
-// pseudocoding is using plain language to discribe what the code is doing. We should use it to help process the logic you indend to execute.
-// 6. what percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
-// you should spend over half the time planning (over 60%), the time should be spent in solving the actual coding should take less time.
+
+
+// B STRINGS
+let firstVariable = "Hello World";
+firstVariable = 32;
+let secondVariable = firstVariable;
+secondVariable = "Nice!"
+console.log(firstVariable);
+let yourName = "Mary";
+let greeting = `Hello, my name is ${yourName}`;
+console.log(greeting);
+
 
 
 
