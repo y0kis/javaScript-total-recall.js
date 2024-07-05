98323c0e074088673740cbef13e78b64b4537d8e
y0kis <yokish1986@gmail.com>
2024-07-05 11:49:28 -0400

    Q & A

---
+++ javaScript-total-recall.js
@@ -0,0 +1,25 @@
+//variable and datatypes
+// 1. How do we assign a value to a variable? 
+// We assign a value to a variable by using the assignment (=) operatorto assign a value to varianble or property.
+//2. How do we change the value of a variable?
+// the first and easiest way of changing a value of a variable is simply to reaasign it:
+//example, let numberOfCats = 3; numberOfCats = 4;
+//3. what are declare, assign, define:
+// declare use var(only in older programs), let, const
+// assign using the assignment opperator
+// define a variable is to give the variable a value.
+//5. what is pseudocoding and why should you do it?
+// pseudocoding is using plain language to discribe what the code is doing. We should use it to help process the logic you indend to execute.
+// 6. what percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// you should spend over half the time planning (over 60%), the time should be spent in solving the actual coding should take less time.
+
+
+
+
+
+
+
+
+
+
+
