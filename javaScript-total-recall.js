c143fbb9866cb31639fe86d071bdeacb7c76f5a1
y0kis <yokish1986@gmail.com>
2024-07-05 23:40:01 -0400

    BOOLEANS

--- javaScript-total-recall.js
+++ javaScript-total-recall.js
@@ -2,27 +2,69 @@
 // 1. How do we assign a value to a variable? 
 // We assign a value to a variable by using the assignment (=) operatorto assign a value to varianble or property.
 //2. How do we change the value of a variable?
-// the first and easiest way of changing a value of a variable is simply to reaasign it:
+// The first and easiest way of changing a value of a variable is simply to reaasign it:
 //example, let numberOfCats = 3; numberOfCats = 4;
+// 3. What are declare, assign and define?
+// Declare use var(only in older programs), let,const
+//Assign using the assignment aperator 
+//Define a variable is to give the varaible a value.
+//5.What is pseudocoding and why should you do it?
+//Pseudocoding is using plain language to describe what the code is doing. We should use it to help process the logic you intend to execute.
+//6.What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+//You should spend over half the time planning (over 60%), the time should be spent in solving the actual coding should take less time.
 
 
 // B STRINGS
-let firstVariable = "Hello World";
-firstVariable = 32;
-let secondVariable = firstVariable;
-secondVariable = "Nice!"
-console.log(firstVariable);
-let yourName = "Mary";
-let greeting = `Hello, my name is ${yourName}`;
-console.log(greeting);
+// let firstVariable = "Hello World";
+// firstVariable = 32;
+//let secondVariable = firstVariable;
+//secondVariable = "Nice!"
+//console.log(firstVariable);
+//let yourName = "Mary";
+//let greeting = `Hello, my name is ${yourName}`;
+//console.log(greeting);
 
 // BOOLEANS
+///const a = 4;
+//const b = 53;
+//const c = 57;
+//const d = 16;
+//const e = 'Kevin';
+//console.log(a != b);
+
+//BOOLEANS
 const a = 4;
 const b = 53;
 const c = 57;
 const d = 16;
 const e = 'Kevin';
 console.log(a != b);
+console.log(c != d);
+console.log('Name' == 'Name');
+
+//FOR THE NEXT TWO, USE ONLY && OR ||
+console.log(true || false);
+console.log(false && false && false || true);
+console.log(false == false);
+console.log(e == 'Kevin');
+console.log(a < b != c);
+console.log(a == a != d);
+console.log(48 == '48');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
